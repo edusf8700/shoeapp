319d7c2087a4d63d544598c18ecc7a64ddc7caed
Eduardo Freitas <edusf8700@gmail.com>
2023-02-26 14:44:27 -0300

    chore:nativebase and google fonts configuration

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/theme';
+import { Home } from './src/screens/Home';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar style="auto" />
-    </View>
+      { fontsLoaded ? <Home/> : <Loading/> }  
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "native-base";
+
+export function Home() {
+  return(
+    <Center flex={1} bg="gray.700">
+      <Text fontSize="xl" color="green.500">Home</Text>
+    </Center>
+  );
+}
